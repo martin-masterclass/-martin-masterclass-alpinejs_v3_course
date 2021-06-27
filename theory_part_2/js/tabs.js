73bec8600b846997f825f6747a9886485fc70829
Martin Eberth <mkrebseberth@gmail.com>
2021-06-27 12:18:35 +0200

    Refactor Tabs Example

--- theory_part_2/js/tabs.js
+++ theory_part_2/js/tabs.js
@@ -1,6 +1,6 @@
 let dynamicTabs = () => {
     return {
-        tab: 1,
+        tab: 0,
         tabClasses : 'px-5 py-3 rounded-xl rounded-b cursor-pointer ',
         tabNormalColors: 'bg-gray-200 hover:bg-gray-300',
         tabHiColor: 'bg-gray-400',
