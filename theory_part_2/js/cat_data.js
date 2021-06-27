d77c0a6dc5af5c431bc5ea009181e3456c66fd09
Martin Eberth <mkrebseberth@gmail.com>
2021-06-27 12:56:31 +0200

    x-init: Inject data in your alpine component

---
+++ theory_part_2/js/cat_data.js
@@ -0,0 +1,21 @@
+let cat_1 = [
+    {
+        title : "Description",
+        body: "<div class='flex'><img class='mr-2' src='https://placekitten.com/g/150/150'><span>Super nice cat!</span></div>"
+    },
+    {
+        title : "Specifics",
+        body: "<b>Weight:</b> 3kg"
+    },
+];
+
+let cat_2 = [
+    {
+        title : "Description",
+        body: "<div class='flex'><img class='mr-2' src='https://placekitten.com/g/200/150'><span>Super beautiful cat!</span></div>"
+    },
+    {
+        title : "Specifics",
+        body: "<b>Weight:</b> 4kg"
+    },
+];
